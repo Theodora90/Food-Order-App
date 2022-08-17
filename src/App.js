47533deba501ee720a8d3c2c1c3c4d89885aec68
theodora90 <theodora@lifebank.ng>
2022-08-17 13:03:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import Header from "./components/LAYOUT/header";
+import Meals from "./components/MEALS/meals";
+import Cart from "./components/CART/cart";
+import CartProvider from "./store/cartProvider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
